c43fa5ae282b6240344c083d0a558811972bf633
lukas8219 <lucas.c4d@gmail.com>
2025-06-17 20:55:42 -0300

    [threads] added bfs compare between mirror, safe and non-safe BST

--- threads/30-2-binary-tree-pthreads.c
+++ threads/30-2-binary-tree-pthreads.c
@@ -78,10 +78,10 @@ atomic_int volatile non_safe_counter = 0;
 
 struct t_node volatile *safe_three;
 static pthread_mutex_t three_mutex = PTHREAD_MUTEX_INITIALIZER;
-atomic_int volatile safe_counter = 0;
+int volatile safe_counter = 0;
 struct t_node volatile *safe_three_mirror;
 static pthread_mutex_t three_mutex_mirror = PTHREAD_MUTEX_INITIALIZER;
-atomic_int volatile safe_counter_mirror = 0;
+int volatile safe_counter_mirror = 0;
 int MAX_WORKERS = 500;
 
 void* insert_into_non_safe(void* args){
@@ -97,34 +97,34 @@ void* insert_into_non_safe(void* args){
 
 void* insert_into_safe(void* args){
   int* shuffled_array = (int*) args;
-  int counter = atomic_load(&safe_counter);
-  while (counter < size) {
-    int s = pthread_mutex_lock(&three_mutex);
-    if (s != 0) {
-      exit(1);
-      return 0;
+  //unoptimized way - doing a hotloop - maybe a pthread_cond_wait
+  while (1) {
+    pthread_mutex_lock(&three_mutex);
+    if (safe_counter >= size) {
+      pthread_mutex_unlock(&three_mutex);
+      break;
     }
-    int value = shuffled_array[counter];
+    int value = shuffled_array[safe_counter];
     insert((struct t_node**)&safe_three, value);
-    s = pthread_mutex_unlock(&three_mutex);
-    counter = atomic_fetch_add(&safe_counter, 1);
+    ++safe_counter;
+    pthread_mutex_unlock(&three_mutex);
   }
   return 0;
 }
 
 void* insert_into_mirror(void* args){
   int* shuffled_array = (int*) args;
-  int counter = atomic_load(&safe_counter_mirror);
-  while (counter < size) {
-    int s = pthread_mutex_lock(&three_mutex_mirror);
-    if (s != 0) {
-      exit(1);
-      return 0;
+  //unoptimized way - doing a hotloop - maybe a pthread_cond_wait
+  while (1) {
+    pthread_mutex_lock(&three_mutex_mirror);
+    if (safe_counter_mirror >= size) {
+      pthread_mutex_unlock(&three_mutex_mirror);
+      break;
     }
-    int value = shuffled_array[counter];
+    int value = shuffled_array[safe_counter_mirror];
     insert((struct t_node**)&safe_three_mirror, value);
-    s = pthread_mutex_unlock(&three_mutex_mirror);
-    counter = atomic_fetch_add(&safe_counter_mirror, 1);
+    ++safe_counter_mirror;
+    pthread_mutex_unlock(&three_mutex_mirror);
   }
   return 0;
 }
@@ -159,14 +159,11 @@ int compare_arrays(int* arr0, int* arr1, int size){
 }
 
 int main(){
-  struct t_node *tree = create_empty_node(0);
   non_safe_three = create_empty_node(0);
   safe_three = create_empty_node(0);
+  safe_three_mirror = create_empty_node(0);
 
   int* shuffled_array = create_shuffled_array(size);
-  for(int i = 0; i < size; i++){
-    insert(&tree, shuffled_array[i]);
-  }
 
   pthread_t workers_non_safe[MAX_WORKERS];
 
@@ -191,17 +188,6 @@ int main(){
     pthread_join(workers_safe[i], NULL);
   }
 
-  int resulting_array[size];
-  int resulting_array_non_safe[size];
-  tree_to_array_bfs((struct t_node*)safe_three, resulting_array, size);
-  tree_to_array_bfs((struct t_node*)non_safe_three, resulting_array_non_safe, size);
-
-  int code = compare_arrays(resulting_array, resulting_array_non_safe, size);
-
-  if (code != 0) {
-    printf("Safe and non-safe are not equal - expected\n");
-  }
-
   pthread_t workers_safe_mirror[MAX_WORKERS];
   for(int i=0; i < MAX_WORKERS; i++){
     pthread_t worker;
@@ -213,8 +199,19 @@ int main(){
     pthread_join(workers_safe_mirror[i], NULL);
   }
 
+  int resulting_array[size];
+  int resulting_array_non_safe[size];
   int resulting_array_safe_2[size];
+  tree_to_array_bfs((struct t_node*)safe_three, resulting_array, size);
+  tree_to_array_bfs((struct t_node*)non_safe_three, resulting_array_non_safe, size);
   tree_to_array_bfs((struct t_node*)safe_three_mirror, resulting_array_safe_2, size);
+
+  int code = compare_arrays(resulting_array, resulting_array_non_safe, size);
+
+  if (code != 0) {
+    printf("Safe and non-safe are not equal - expected\n");
+  }
+
   code = compare_arrays(resulting_array, resulting_array_safe_2, size);
 
   if (code != 0) {
